fbca8e842c65c86c6b99b610f67aac24a9179db3
Guilherme Penedo <guipedroborges@gmail.com>
2025-10-21 21:38:33 +0100

    testes

---
+++ main.py
@@ -0,0 +1,22 @@
+import httpx
+
+server = "router.project-osrm.org"
+
+my_coords = "9.139806,38.736889"
+
+def get_request(service, version,profile, coordinates, format, options):
+    url = f"http://{server}/{service}/{version}/{profile}/{coordinates}.{format}"
+
+    if options:
+        url += "?" + "&".join(options)
+
+    response = httpx.get(url)
+    response.raise_for_status()  # opcional: lança erro se HTTP != 200
+    return response.json()
+
+def get_nearest_service(profile, coordinates, number):
+    return get_request("nearest", "v1", profile, coordinates, "json", ["number=" + str(number)])
+
+
+data = get_nearest_service("driving", my_coords, 1)
+print(data)
